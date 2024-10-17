285b7ac64ccd7f6de87879ad6e4beedf34c48cca
Pranesh Raghumu vedhapandiyan <pvedhapandiyan@freshworks.com>
2024-10-17 22:58:27 +0530

    ham

---
+++ Javascript.js
@@ -0,0 +1,5 @@
+// Toggle hamburger menu
+document.getElementById('hamburger').addEventListener('click', function() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active'); // Toggle the 'active' class
+});
